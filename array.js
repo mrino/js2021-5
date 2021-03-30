0a821fc7963e1426b848f8c8f3e8483a8f6a0cbc
mrino <ehdwn1173@naver.com>
2021-03-30 16:27:03 +0900

    ㅇㄴㅀㄴㅇㅀ

---
+++ array.js
@@ -0,0 +1,13 @@
+let i = 0;
+let array = [52,273,32,65,103];
+
+while(i < array.length)
+{
+    console.log(i+1 +"번째 출력: "+ array[i]);
+    i++;
+}
+i = 0;
+for(i; i< array.length; i++)
+{
+    console.log(i+1 +"번째 출력: "+ array[i]);
+}
