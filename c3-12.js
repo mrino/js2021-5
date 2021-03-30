1ef3e74867c4ec002a00f0a06346fed26ab94cdc
mrino <ehdwn1173@naver.com>
2021-03-30 15:20:15 +0900

    우라라라라

---
+++ c3-12.js
@@ -0,0 +1,3 @@
+let num = 30;
+
+console.log(num % 2 == 0 ? "짝수" : "홀수");
--- code3-10.js
+++ code3-10.js
@@ -1,11 +1,30 @@
-let input = 2;
+let date = new Date();
 
-switch(input % 2)
+console.log(date.getMonth())
+
+switch(date.getMonth()+ 1)
 {
-    case 0:
-        console.log("짝수입니다");
-        break;
+    case 12:
     case 1:
-        console.log("홀수입니다");
+    case 2:
+        console.log("겨울");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log("봄");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log("여름");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log("가을");
+        break;
+    default:
+        console.log("일단 같은 세계관은 아닌듯")
         break;
 }
