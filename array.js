5b757b8da58d0407ee3c1dcb410a3e213c8f75e7
mrino <ehdwn1173@naver.com>
2021-04-06 16:08:33 +0900

    push psuh

--- array.js
+++ array.js
@@ -24,3 +24,30 @@ for(let item of arr)
 {
     console.log(item);
 }
+
+
+let foo = [1,2,3,4,5];
+console.log(foo.length);
+console.log(foo);
+foo.push(13);
+console.log(foo.length);
+console.log(foo);
+foo.pop();
+console.log(foo.length);
+console.log(foo);
+foo.push(13);
+console.log(foo.length);
+console.log(foo);
+
+console.log("//////");
+console.log(foo.length);
+for(let i in foo)
+{
+    console.log(i);
+    foo.pop();
+    console.log(foo);
+}
+
+
+console.log("ddd")
+console.log(foo);
