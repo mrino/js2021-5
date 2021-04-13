dfd338c8382498659739f419ffc1fa56e0f60f8e
mrino <ehdwn1173@naver.com>
2021-04-13 15:35:44 +0900

    4141414141414141414

--- ch05.js
+++ ch05.js
@@ -30,6 +30,8 @@ let fun3 = function(){
 fun3();
 console.log(fun3);
 
+
+//함수의 기본형태
 function power(x){
     let foo;
     foo = x * x;
@@ -40,7 +42,7 @@ console.log(power(10));
 console.log(power(1000));
 
 
-
+//변수가 여러 개인 함수
 function multply(x,y){
     let foo;
     foo = x * y;
@@ -49,5 +51,29 @@ function multply(x,y){
 
 console.log(multply(3,4));
 console.log(multply(4,2));
+
+//리턴이 없는 함수
+function doprint(message){
+    console.log(`"${message}"(이)라고 말하셨습니다`);
+}
+
+doprint("안녕하세요");
+doprint("감사해요");
+
+
+//가우스 연산
+
+function gaussum(min,max)
+{
+    let output = 0;
+    for(let i = min; i <= max; i++)
+    {
+        output += i;
+    }
+    return output;
+}
+
+console.log(gaussum(1,100));
+
 
 
